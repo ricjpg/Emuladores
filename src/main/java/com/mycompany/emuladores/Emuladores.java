028ad0b6a367a8cad5ea805f7ae8856979b2e094
unknown <ricardoguardiolahn@gmail.com>
2023-02-14 21:11:50 -0600

    first commit

---
+++ src/main/java/com/mycompany/emuladores/Emuladores.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.emuladores;
+
+/**
+ *
+ * @author Ricardo Guardiola
+ */
+public class Emuladores {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        Menu me = new Menu();
+        me.setVisible(true);
+        
+        
+    }
+}
